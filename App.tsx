5f1cfd9a6ce7cca587183f0938d56b198f3ecb03
Ana Alexandra Isaías <aalexani@emeal.nttdata.com>
2023-05-23 13:06:32 +0100

    Project initialization

---
+++ App.tsx
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <View></View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
